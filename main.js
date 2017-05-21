d054f86b720b11a03c308074e169c35d3a7a1306
Guillaume <deroguerre@gmail.com>
2017-05-21 20:27:55 +0200

    test with nodejs

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 //REQUIRES
+var express = require('express');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 var Player = require("./class/player.js");
 
 //GAME
 var oPlayer1 = new Player(5, 5, 6);
 
+
+var app = express();
+
+// app.get('/index.html', function(req, res) {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Vous êtes à l\'accueil');
+// });
+
+app.use('/', express.static(__dirname + '/'));
+
+app.listen(8080);
+
+const { window } = new JSDOM(`<!DOCTYPE html>`);
+const $ = require('jQuery')(window);
 //RENDER
-$(function () {
 
     $('#hp').html(oPlayer1.hp);
 
     /*affiche la barre de statuts avec effet fadeIn*/
-    $( "#statusBar" ).fadeIn( "slow", function() {
+    $("#statusBar").fadeIn("slow", function () {
 
     });
-});
---
+++ main2.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $('#hp').html(oPlayer1.hp);
+
+    /*affiche la barre de statuts avec effet fadeIn*/
+    $("#statusBar").fadeIn("slow", function () {
+
+    });
+
+});
